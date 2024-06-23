ba2e7d7e511041615f45aaa06a02e80977791ae0
hemant0621 <hemant.kumar.2335.h@gmail.com>
2024-06-23 08:08:51 +0530

    added routes

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Landing() {
+    return (
+        <>
+            <div className="flex justify-center bg-black text-white h-screen items-center text-5xl">
+                HackiFyi
+            </div>
+        </>
+    )
+}
+
+export default Landing
